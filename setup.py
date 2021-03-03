36dea154cc7574f8e8d7464b5f8829cb9e2f6a36
Olle Vidner <olle@vidner.se>
2021-03-03 09:44:09 +0100

    Update/remove version reqs

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
-    python_requires=">=3.8, <4",
+    python_requires=">=3.7, <4",
     setup_requires=["setuptools_scm"],
-    install_requires=["openmdao>=3.4", "numpy", "deap"],
+    install_requires=["openmdao", "numpy", "deap"],
 )
